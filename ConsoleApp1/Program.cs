bc4e6812916f406bc04ec48a9c8dd0b094dce7f0
Anders Sjöberg <anders.sjoberg@sileon.com>
2023-11-29 19:50:40 +0100

    Dynamic json objects read

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Newtonsoft.Json.Linq;
+
+var json1 = """
+    {
+        "name": "John",
+        "age": 30,
+        "cars": [
+            "Ford",
+            "BMW",
+            "Fiat"
+        ]
+    }
+    """;
+
+var json2 = """
+    {
+        "account": 8987,
+        "name": "Ruben",
+        "age": 72,
+        "cars": [
+            "Skoda",
+            "VW",
+            "Auto Bianchi"
+        ]
+    }
+    """;
+
+//function to read dynamic json
+static dynamic? ReadJson(string json)
+{
+    return JObject.Parse(json);
+}
+
+var thing1 = ReadJson(json1);
+var thing2 = ReadJson(json2);
+
+Console.WriteLine(thing1?.account ?? "No account found");
+Console.WriteLine(thing2?.account ?? "No account found");
